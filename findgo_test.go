0d72854774c9185f9a55dc752048a5f3bf7dd026
Walter Vargas <waltervargas@linux.com>
2022-10-13 02:22:38 +0200

    Add benchmarks

--- findgo_test.go
+++ findgo_test.go
@@ -1,6 +1,8 @@
 package findgo_test
 
 import (
+	"archive/zip"
+	"os"
 	"testing"
 	"testing/fstest"
 
@@ -21,4 +23,48 @@ func TestFilesInMemory(t *testing.T) {
 		t.Errorf("want %d, got %d", want, got)
 	}
 
+}
+
+func TestFilesOnDisk(t *testing.T) {
+	t.Parallel()
+	fsys := os.DirFS("findgo")
+	want := 4
+	got := findgo.Files(fsys)
+	if want != got {
+		t.Errorf("want %d, got %d", want, got)
+	}
+}
+
+func BenchmarkFilesOnDisk(b *testing.B) {
+	fsys := os.DirFS("findgo")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		findgo.Files(fsys)
+	}
+}
+
+func BenchmarkFilesInMemory(b *testing.B) {
+	fsys := fstest.MapFS{
+		"file.go":                {},
+		"subfolder/subfolder.go": {},
+		"subfolder2/another.go":  {},
+		"subfolder2/file.go":     {},
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		findgo.Files(fsys)
+	}
+}
+
+func TestFilesInZIP(t *testing.T) {
+	t.Parallel()
+	fsys, err := zip.OpenReader("findgo.zip")
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := 4
+	got := findgo.Files(fsys)
+	if want != got {
+		t.Errorf("want %d, got %d", want, got)
+	}
 }
